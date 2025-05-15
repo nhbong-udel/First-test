f47ccfdddbc3181d51d66565d3f273bac2fb5567
nhbong-udel <nhbong@udel.edu>
2025-05-15 17:40:40 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Section Title",
-  "body": " Section Title  Text of section.  This is a paragraph that has emphasized text. Really? I love pretext. Sine is written . .    A graph is bla bla bla     Consider the graph    Note that   Here is my list of animals:  Shark  Sloth  Slork    My favorite numbers, in no particular order, happen to be:  1  2  3     Integration by parts    Solve the following integrals          LIATE    lalala           LIATE    lalala         f(x)=2.5-x^2\/2  a = 1       (a,f(a))          graph={1:[3,4,5,5],2:[4,5],3:[4,5],6:[2,4]}             f(x)=2.5-x^2\/2  a = 1       (a,f(a))        lala  "
+  "body": " Section Title  Text of section.  This is a paragraph that has emphasized text. Really? I love pretext. Sine is written . .    A graph is bla bla bla     Consider the graph    Note that   Here is my list of animals:  Shark  Sloth  Slork    My favorite numbers, in no particular order, happen to be:  1  2  3     Integration by parts    Solve the following integrals          LIATE    lalala           LIATE    lalala         f(x)=2.5-x^2\/2  a = 1       (a,f(a))          graph={1:[3,4,5,5],2:[4,5],3:[4,5],6:[2,4]}             f(x)=2.5-x^2\/2  a = 1       (a,f(a))        lala     "
 },
 {
   "id": "def-",
@@ -62,6 +62,42 @@ var ptx_lunr_docs = [
   "number": "1.1.4",
   "title": "",
   "body": "    f(x)=2.5-x^2\/2  a = 1       (a,f(a))       "
+},
+{
+  "id": "sec-section-name-14",
+  "level": "2",
+  "url": "sec-section-name.html#sec-section-name-14",
+  "type": "Activity",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  "
+},
+{
+  "id": "M315Ch1S2",
+  "level": "1",
+  "url": "M315Ch1S2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Graph Isomorphism",
+  "body": " Graph Isomorphism    Two graphs and are isomorphic if there is a bijective function such that . is called an isomorphism from to       Let and be two graphs. If and are isomorphic, then   The graphs and have the same degree sequence.   The graphs and have the same number of triangles.      "
+},
+{
+  "id": "def-Isomorphism",
+  "level": "2",
+  "url": "M315Ch1S2.html#def-Isomorphism",
+  "type": "Definition",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  Two graphs and are isomorphic if there is a bijective function such that . is called an isomorphism from to    "
+},
+{
+  "id": "prop-GraphIsomorphism",
+  "level": "2",
+  "url": "M315Ch1S2.html#prop-GraphIsomorphism",
+  "type": "Proposition",
+  "number": "1.2.2",
+  "title": "",
+  "body": "  Let and be two graphs. If and are isomorphic, then   The graphs and have the same degree sequence.   The graphs and have the same number of triangles.     "
 },
 {
   "id": "backmatter-2",
